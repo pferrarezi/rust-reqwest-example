de0d906728b5f059b2a4ffeeebeeecb4ab4cdd05
Pedro Ferrarezi <peferrarezi@gmail.com>
2020-07-01 22:09:04 -0400

    fist commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+extern crate reqwest;
+
+fn main() {
+    let url = "https://puppygifs.tumblr.com/api/read/json";
+    let mut req = reqwest::get(url).expect("Erro na requisição GET");
+    let req_text;
+    if req.status().is_success() {
+        req_text = req.text().expect("Erro obtendo response text");
+        println!("body = {}", req_text);
+    } else {
+        // req_text = String::from("lolo");
+        print!("Status: {}", req.status())
+    }
+}
